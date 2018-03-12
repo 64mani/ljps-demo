d2c50568dce50cb991e5f8a74814e2e8992a261a
Manish Kumar <manishkumar@schoolmitra.com>
2018-03-12 13:09:57 +0530

    new updates home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { AdmissionComponent } from './admission/admission.component';
+import { HouseComponent } from './house/house.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/homePage', pathMatch: 'full' },
   { pathMatch: 'full', path: 'homePage', component: HomePageComponent },
+  { pathMatch: 'full', path: 'admission', component: AdmissionComponent },
+  { pathMatch: 'full', path: 'house', component: HouseComponent},
 
 ];
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    AdmissionComponent,
+    HouseComponent
   ],
   imports: [
     BrowserModule,
