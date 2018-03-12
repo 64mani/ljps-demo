1c522b1e44225847e4364fef2e0ab037858a9c5f
Manish Kumar <manishkumar@schoolmitra.com>
2018-03-12 09:53:53 +0530

    new updates home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/homePage', pathMatch: 'full' },
+  { pathMatch: 'full', path: 'homePage', component: HomePageComponent },
 
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
